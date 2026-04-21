3e9e2c82856b35cca5036c42d857bf8ad2570237
陨落基围虾 <3161880837@qq.com>
2026-04-22 06:48:48 +0800

    1

--- write.py
+++ write.py
@@ -14,22 +14,19 @@ article_current_content = article_output.read()
 apikey = os.environ.get("AI_APIKEY")
 generator_id = os.environ.get("AI_GENERATOR")
 
-if generator_id and apikey:
-    generate = generators[generator_id]
-    if generate:
-        print("当前文段：", article_current_content)
-        text = generate(article_current_content, system_prompt, apikey)
-        if text:
-            response = ConfigOutput.model_validate_json(text)
-            print("续写：", response.next_paraphrase)
-            print("自评：", response.self_comment)
-            article_output.write(f"\n{response.next_paraphrase}")
-        else:
-            print("写不出来")
-            sys.exit(1)
-    else:
-        print("生成器不对")
-        sys.exit(1)
-else:
+if not (generator_id and apikey):
     print("没写配置")
     sys.exit(1)
+generate = generators[generator_id]
+if not generate:
+    print("生成器不对")
+    sys.exit(1)
+print("当前文段：", article_current_content)
+text = generate(article_current_content, system_prompt, apikey)
+if not text:
+    print("写不出来")
+    sys.exit(1)
+response = ConfigOutput.model_validate_json(text)
+print("续写：", response.next_paraphrase)
+print("自评：", response.self_comment)
+article_output.write(f"\n{response.next_paraphrase}")
